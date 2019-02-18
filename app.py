bc8ca5485ba87ed52b0645bcceffcb1f6cb7c5ce
alam <alam030995>
2019-02-18 16:27:32 +0700

    nambah kirim ke telegram

--- app.py
+++ app.py
@@ -3,10 +3,23 @@ import datetime
 import sqlite3
 import paho.mqtt.client as mqtt
 import time
+import telegram_send as ts
+import requests
 
 
 app = Flask(__name__)
+def telegram_bot_sendtext(bot_message):
+    
+    bot_token = '609109760:AAFzx05CJ96jT26WLZVtZC4rv0D-uOJN6TU'
+    bot_chatID = '-379105000'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
+    response = requests.get(send_text)
+
+    return response.json()
+    
+
+test = telegram_bot_sendtext("Testing Telegram bot")
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -22,9 +35,11 @@ def on_message(client, userdata, msg):
     c=conn.cursor()
     if str(msg.payload) == "b'KOSONG'" :
       print("KOSONG")
+      telegram_bot_sendtext("GALON HABIS COOOY")
       c.execute("INSERT INTO data_galon (status,waktu) VALUES ('KOSONG',datetime('now','localtime'))")
     elif str(msg.payload) == "b'PENUH'" :
       print("PENUH")
+      telegram_bot_sendtext("TERIMA KASIH MASBRO")
       c.execute("INSERT INTO data_galon (status,waktu) VALUES ('PENUH',datetime('now','localtime'))")
     conn.commit()
     conn.close()
